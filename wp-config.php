a0ba1d13cd537096ac4d11059cfde46cd1704c0f
Česlav Przywara <cp@bluechip.at>
2017-06-01 12:42:07 +0200

    WP CLI compatibility

--- wp-config.php
+++ wp-config.php
@@ -4,7 +4,7 @@
  *
  * @link https://codex.wordpress.org/Editing_wp-config.php
  * @package WordPress
- * @version 20170510
+ * @version 20170601
  */
 
 /**
@@ -27,47 +27,40 @@
  * Default settings that are common to all environments can exist in wp-config/default.php
  */
 
+/** Absolute path to the WordPress directory. It is defined at the bottom of wp-config-sample.php, but it is needed earlier here. */
+if (!defined('ABSPATH')) {
+	define('ABSPATH', dirname(__FILE__) . '/');
+}
+
 // Try environment variable 'WP_ENV'
-if ( getenv('WP_ENV') !== false ) {
+if (getenv('WP_ENV') !== false) {
 	// Filter non-alphabetical characters for security
 	define('WP_ENV', preg_replace('/[^a-z]/', '', getenv('WP_ENV')));
 }
 
-// If WordPress has been bootstrapped via WP-CLI, detect environment from --env=<environment> argument
-if ( PHP_SAPI === "cli" && defined('WP_CLI_ROOT') ) {
-	foreach ($argv as $arg) {
-		if (preg_match('/--env=(.+)/', $arg, $m)) {
-			define('WP_ENV', $m[1]);
-		}
-	}
-}
-
 // Make sure HTTP_HOST is defined when running via WP CLI
 // https://make.wordpress.org/cli/handbook/common-issues/#php-notice-undefined-index-on-_server-superglobal
-if ( defined('WP_CLI') && WP_CLI && !isset($_SERVER['HTTP_HOST']) ) {
-    // TODO: This effectively forces production environment, if --env is undefined (see above)
-    $_SERVER['HTTP_HOST'] = 'wp-cli.org';
+if (defined('WP_CLI') && WP_CLI && !isset($_SERVER['HTTP_HOST'])) {
+	// Environment must be defined at this point, otherwise hostname detection below will always fall back to production.
+	if (!defined('WP_ENV')) {
+		die("WordPress Multi-Environment Config Error: Neither HTTP_HOST nor WP_ENV is defined.\n");
+	}
+	$_SERVER['HTTP_HOST'] = 'wp-cli.org';
 }
 
 // If no environment set at this point, set environment based on hostname
-if ( !defined('WP_ENV') ) {
-	include 'wp-config/env.php';
+if (!defined('WP_ENV')) {
+	require_once ABSPATH . 'wp-config/env.php';
 }
 
 // Load default config
-include 'wp-config/default.php';
+require_once ABSPATH . 'wp-config/default.php';
 // Load config file for current environment
-include 'wp-config/' . WP_ENV . '.php';
+require_once ABSPATH . 'wp-config/' . WP_ENV . '.php';
 
 /** End of WordPress Multi-Environment Config **/
 
-
 /* That's all, stop editing! Happy blogging. */
 
-/** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') ) {
-	define('ABSPATH', dirname(__FILE__) . '/');
-}
-
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+require_once ABSPATH . 'wp-settings.php';
