675b22355acf48e7c25a2650c2ae8b568e25e664
Česlav Przywara <cp@bluechip.at>
2019-08-19 15:53:47 +0200

    Update development config
    Disable error handling introduced in WP 5.2.
    Load unminified core CSS and JS assets.

--- wp-config/development.php
+++ wp-config/development.php
@@ -21,6 +21,12 @@ define('DB_HOST', 'localhost');
 
 
 /**
+ * Disable fatal error handler in development.
+ * @link https://make.wordpress.org/core/2019/04/16/fatal-error-recovery-mode-in-5-2/
+ */
+define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
+
+/**
  * During development, changes to posts and pages are frequent and the database
  * quickly becomes cluttered with revisions...
  */
@@ -31,3 +37,8 @@ define('WP_POST_REVISIONS', false);
  * at least for me as a developer...
  */
 define('AUTOSAVE_INTERVAL', 300);
+
+/**
+ * Force WordPress to use non-minified versions of core CSS and JavaScript.
+ */
+define('SCRIPT_DEBUG', true);
